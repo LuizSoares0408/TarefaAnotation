b5ac07f6254fc0d6e4893301eccef8855493aab2
LuizSoares0408 <luiz.h.g.soares@outlook.com>
2025-09-02 16:28:18 -0300

    Ajustes

--- src/ClasseComAnotacao.java
+++ src/ClasseComAnotacao.java
@@ -1,3 +1,6 @@
+@Tabela(TabelaNome = "Tabela")
 public class ClasseComAnotacao {
 
+    @Tabela(TabelaNome = "Tabela1")
+    private String nome;
 }
--- src/Tabela.java
+++ src/Tabela.java
@@ -1,3 +1,3 @@
-public class Tabela {
-
+public @interface Tabela {
+    String TabelaNome();
 }
